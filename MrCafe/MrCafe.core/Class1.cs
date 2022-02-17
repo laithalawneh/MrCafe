d904705c16c99e2f8ff484bbb9b0fb376a657203
laith <79411552+laithalawneh@users.noreply.github.com>
2022-02-18 01:10:09 +0200

    Delete MrCafe directory

--- MrCafe/MrCafe.core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MrCafe.core
-{
-    public class Class1
-    {
-    }
-}
--- MrCafe/MrCafe.infra/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MrCafe.infra
-{
-    public class Class1
-    {
-    }
-}
